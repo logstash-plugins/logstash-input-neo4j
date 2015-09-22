aea0ac49bca7fb21c0b86642a2c3e5cdcf9e1021
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2015-09-22 10:21:03 +0200

    Make the integration specs to work in a predictible fashion by:
    * cleanup former neo4j database fixtures used for testing
    * fix to be working in the 4.x neo4jrb branch
    * make the client integration test work with dummy database created just for test purpouses
    
    Fixes #4

--- spec/inputs/neo4j-client_spec.rb
+++ spec/inputs/neo4j-client_spec.rb
@@ -2,27 +2,25 @@ require_relative "../spec_helper"
 
 describe "Neo4jrb::Client", :integration => true do
 
-
-  let(:path)   { "spec/fixtures/graph.db" }
-  let(:client) { Neo4jrb::Client.open(path) }
+  let(:client) { Neo4jrb::Client.open(temp_db_path) }
 
   it "can count all nodes within the database" do
-    expect(client.count_nodes).to eq(171)
+    expect(client.count_nodes).to eq(2)
   end
 
   context "while running a query" do
-    let(:query) { "MATCH (p:`Person`)-->(m:`Movie`) WHERE m.released = 2000 RETURN *" }
+    let(:query) { "MATCH (p:`person`)-->(m:`person`) WHERE p.name = 'personA' RETURN *" }
 
     subject(:resultset) { client.execute_query(query) }
 
     it "return the right amount of data" do
-      expect(resultset.count).to eq(24)
+      expect(resultset.count).to eq(1)
     end
 
     it "return the right type of data as array" do
        resultset.each do |result|
          result.map! { |e| e.labels.first }.flatten
-         expect(result).to eq([:Movie, :Person])
+         expect(result).to eq([:person, :person])
        end
     end
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,39 @@
 require "logstash/devutils/rspec/spec_helper"
 require "logstash/inputs/neo4j"
 require "logstash/inputs/neo4j-client"
-require 'ostruct'
+require "ostruct"
+require "fileutils"
 
 def load_fixture(name)
   IO.read("spec/fixtures/#{name}")
+end
+
+def temp_db_path
+  File.join(Dir.tmpdir, "graphdb")
+end
+
+def build_database(path)
+  session = ::Neo4j::Session.open(:embedded_db, path, :auto_commit => true)
+  session.start
+  Neo4j::Transaction.run do
+    nodeA = Neo4j::Node.create({name: 'personA'}, :person)
+    nodeB = Neo4j::Node.create({name: 'personB'}, :person)
+    nodeA.create_rel(:knows, nodeB, since: 2015)
+  end
+end
+
+def remove_database(path)
+  session = Neo4j::Session.current
+  session.shutdown
+  FileUtils.rm_rf(path)
+end
+
+RSpec.configure do |config|
+  config.before(:all) do
+    build_database(temp_db_path)
+  end
+
+  config.after(:all) do
+    remove_database(temp_db_path)
+  end
 end
